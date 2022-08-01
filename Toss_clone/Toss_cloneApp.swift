a8e202c296e6db36149e37ddc33e9d1a2c67a012
tkgka <ksh17171@gmail.com>
2022-08-01 20:34:39 +0900

    Initial Commit

---
+++ Toss_clone/Toss_cloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  Toss_cloneApp.swift
+//  Toss_clone
+//
+//  Created by 김수환 on 2022/08/01.
+//
+
+import SwiftUI
+
+@main
+struct Toss_cloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

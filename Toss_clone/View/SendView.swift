24b51f6fa9cfdd5a4d0fbbe663074dd404c4a5f3
tkgka <ksh17171@gmail.com>
2022-09-18 20:10:51 +0900

    feat(update): fixed bug
    fixed bug: fixed view location bug on send view

--- Toss_clone/View/SendView.swift
+++ Toss_clone/View/SendView.swift
@@ -12,40 +12,49 @@ struct SendView: View {
     @State var AccountNumber:String = ""
     @Environment(\.presentationMode) var presentationMode
     var body: some View {
-        NavigationView{
-            ZStack{
-                Color("BackgroundColor").ignoresSafeArea()
-                VStack(){
-                    Text("어디로 돈을 보낼까요?").font(.title).foregroundColor(Color("FontColor"))
-                        .frame(width: UIScreen.main.bounds.size.width - 50 , alignment: .topLeading)
-                        .padding([.leading ,.bottom], 30)
-                    
-                    ScrollView{
-                        VStack{
-                            Text("계좌번호 입력")
-                                .frame(width: UIScreen.main.bounds.size.width - 50 , alignment: .topLeading)
-                            Rectangle()
-                                .frame(height: 1)
-                                .padding(.leading, 16)
-                                .padding(.trailing, 16)
-                        }.foregroundColor(.gray)
-                    }
-                }
-                .toolbar{
-                    ToolbarItem(placement: .navigationBarLeading) {
-                        if bank != nil {
-                            Button(action: {
-                                presentationMode.wrappedValue.dismiss()
-                            }, label: {
-                                Image(systemName: "chevron.left").foregroundColor(Color("FontColor"))
-                                    .imageScale(.large)
-                                    .padding()
-                            }).accentColor(.black)
+        VStack{
+            NavigationView{
+                ZStack{
+                    Color("BackgroundColor").ignoresSafeArea()
+                    VStack(){
+                        Text("어디로 돈을 보낼까요?").font(.title).bold().foregroundColor(Color("FontColor"))
+                            .frame(width: UIScreen.main.bounds.size.width - 50 , alignment: .topLeading)
+                            .padding([.leading ,.bottom], 30)
+                        
+                        ScrollView{
+                            VStack{
+                                Text("계좌번호 입력")
+                                    .frame(width: UIScreen.main.bounds.size.width - 50 , alignment: .topLeading)
+                                Rectangle()
+                                    .frame(height: 1)
+                                    .padding(.leading, 16)
+                                    .padding(.trailing, 16)
+                            }.foregroundColor(.gray)
                         }
                     }
-                }.navigationBarTitleDisplayMode(.inline)
-                if bank == nil {
-                    TabBar()
+                    .toolbar{
+                        ToolbarItem(placement: .navigationBarLeading) {
+                            if bank != nil {
+                                Button(action: {
+                                    presentationMode.wrappedValue.dismiss()
+                                }, label: {
+                                     Image(systemName: "chevron.left").foregroundColor(Color("FontColor"))
+                                        .imageScale(.large)
+                                        .padding()
+                                }).accentColor(.black)
+                            }
+                        }
+                        ToolbarItem(placement: .navigationBarTrailing){
+                            Button(action: {
+                                print("Tapped")
+                            }, label: {
+                                 Text("")
+                            }).accentColor(.black)
+                        }
+                    }.navigationBarTitleDisplayMode(.inline)
+                    if bank == nil {
+                        TabBar()
+                    }
                 }
             }
         }
